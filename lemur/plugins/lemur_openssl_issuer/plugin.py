876ea77593c6f368d054a7dfc0d28fc004dcd360
Titouan Christophe <titouan.christophe@railnova.eu>
2018-03-23 11:24:37 +0100

    [add] create_certificate method

--- lemur/plugins/lemur_openssl_issuer/plugin.py
+++ lemur/plugins/lemur_openssl_issuer/plugin.py
@@ -6,12 +6,48 @@ from cryptography.hazmat.primitives import hashes, serialization
 
 import os
 import subprocess
+import tempfile
 
 from lemur.plugins.bases import IssuerPlugin
 from lemur.plugins import lemur_openssl_issuer
-from lemur.common.utils import validate_conf
+from lemur.common.utils import validate_conf, parse_certificate
 
 from lemur.certificates.service import create_csr
+
+openssl_cnf = """
+[ ca ]
+default_ca      = CA_default            # The default ca section
+
+[ CA_default ]
+dir            = ./              # top dir
+database       = $dir/index.txt        # index file.
+new_certs_dir  = $dir/newcerts         # new certs dir
+
+certificate    = $dir/certs/ca.cert.pem       # The CA cert
+serial         = $dir/serial           # serial no file
+private_key    = $dir/private/ca.key.pem# CA private key
+RANDFILE       = $dir/private/.rand    # random number file
+
+default_days   = 365                   # how long to certify for
+default_crl_days= 30                   # how long before next CRL
+default_md     = md5                   # md to use
+
+policy         = policy_any            # default policy
+email_in_dn    = no                    # Don't add the email into cert DN
+
+name_opt       = ca_default            # Subject name display option
+cert_opt       = ca_default            # Certificate display option
+copy_extensions = none                 # Don't copy extensions from request
+
+[ policy_any ]
+countryName            = supplied
+stateOrProvinceName    = optional
+organizationName       = optional
+organizationalUnitName = optional
+commonName             = supplied
+emailAddress           = optional
+"""
+
 
 def initialize_CA(basedir, serial_number):
     for sub_dir in ["certs", "crl", "newcerts", "private"]:
@@ -26,11 +62,13 @@ def initialize_CA(basedir, serial_number):
         f.write("unique_subject = no\n")
     open(os.path.join(basedir, "index.txt"), "w").close()
 
+
 def generate_private_key(basedir, key_size):
     subprocess.check_call(["/usr/bin/openssl", "genrsa",
                            "-out", "private/ca.key.pem",
                            "{}".format(key_size)], cwd=basedir)
     subprocess.check_call(["chmod", "400", "private/ca.key.pem"], cwd=basedir)
+
 
 def sign_root_CA(basedir, days, subj):
     subprocess.check_call(["/usr/bin/openssl", "req", "-batch",
@@ -44,7 +82,10 @@ def update_crl(basedir):
     # TODO
     pass
 
+
 SUBJECT = "/C={country}/ST={state}/L={location}/O={organization}/OU={organizational_unit}/emailAddress={owner}/CN={common_name}"
+
+
 class OpensslIssuerPlugin(IssuerPlugin):
     title = 'Openssl issuer'
     slug = 'opensslIssuer'
@@ -52,8 +93,7 @@ class OpensslIssuerPlugin(IssuerPlugin):
     version = lemur_openssl_issuer.VERSION
 
     author = 'Romain Fontaine'
-    author_url = 'https://perdu.com'
-
+    author_url = 'https://github.com/etnarek'
 
     def __init__(self, *args, **kwargs):
         required_vars = [
@@ -65,8 +105,25 @@ class OpensslIssuerPlugin(IssuerPlugin):
         super(OpensslIssuerPlugin, self).__init__(*args, **kwargs)
 
     def create_certificate(self, csr, issuer_options):
-        # requests.get('a third party')
-        pass
+        print("csr: ", csr)
+        print("options: ", issuer_options)
+        ca_name = issuer_options["authority"].name
+        basedir = os.path.join(current_app.config.get("OPENSSL_DIR"), ca_name)
+
+        f = tempfile.NamedTemporaryFile("w", delete=False)
+        f.write(csr)
+        f.close()
+        subprocess.check_call(["/usr/bin/openssl", "ca",
+                               "-batch",
+                               "-config", "openssl.cnf",
+                               "-in", f.name,
+                               "-out", f.name + ".crt"], cwd=basedir)
+        with open(f.name + ".crt") as crt:
+            cert = crt.read()
+
+        parsed_cert = parse_certificate(cert)
+
+        return cert, issuer_options["authority"].authority_certificate.body, parsed_cert.serial_number
 
     def revoke_certificate(self, certificate, comments):
         # requests.put('a third party')
